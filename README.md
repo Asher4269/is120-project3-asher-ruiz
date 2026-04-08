50f26856e4476ac73a8ea7df4ef772cbf6714ce3
Asher Ruiz <asherruiz42@gmail.com>
2026-04-08 00:02:26 -0600

    Initialize Project
    Basic Website Started, work to come

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# is120-project3-asher-ruiz
-Project 3: Supabase
+# IS 120 - Project 3: Supabase
+
+Asher Ruiz
+
+## Supabase
+
+Name: Budget-Pilot
+My 5 columns are: id, User_Name(this would usually be a foreign key), Budget_Name (Also would be better to be a foreign key, but names the group of expenditures in a given budget), Type (Expense, Budget Allotment, or Savings Goal), Amount (The $ Amount of the line item)
+
+## Brief Description of Budget Project:
+
+My goal for this project was to fulfill my goal of creating an interactive budget website utilizing supabase. Even though I am not utilizing foreign keys to simplify tables, I will implement the idea of using user_names and budget_names as keys to access a group of data. The idea is that the user can create budgets in memory based on their name and the name of their budget. They will be able to add line items with individual names and expenditure amounts, which will be displayed in a separate box that lists out all of their expenses in an easy-to-read, beneficial table. It will also use regression to calculate gross income needed based on the listed expenses.
+
+## What I learned while Building:
+
+-
+
+## What was difficult:
+
+-
+
+## Specifically, what did I learn about Supabase and CRUD:
+
+-
+
+## Anything I would like to add or change for Project 3 in future:
+
+-
---
+++ script.js
@@ -0,0 +1,92 @@
+// initialize database
+// const db = supabase.createClient(
+//   "https://picufxzhyglwmqukzyyl.supabase.co",
+//   "sb_publishable_51KQNADsbiEELlQxpjBBPQ_tRauQlrJ",
+// );
+
+const body = document.querySelector("body");
+
+// Display or Alter Budget Name on Display Div
+const budget_name_display = document.querySelector(".budget-name");
+
+// Category Display Divs
+const div_expenses_cat = document.querySelector(".expenses-category");
+const div_budgets_cat = document.querySelector(".budgets-category");
+const div_savings_cat = document.querySelector(".savings-category");
+const div_income_calc_ = document.querySelector(".income-calculator");
+
+// Crud Buttons
+const button_alter_item = document.querySelector(".alter-item");
+const button_delete_item = document.querySelector(".delete-item");
+const button_add_item = document.querySelector(".add-item");
+
+// Database Table Name
+let table_name = "Budget-Line-Items";
+
+// Read in all Data
+async function get_all() {
+  const { data, error } = await db.from(table_name).select("*");
+  console.log(data);
+
+  if (error) {
+    console.error("Error fetching data:", error);
+    return;
+  }
+
+  return data;
+}
+
+// Insert Row(s)
+async function insert_rows(user_name, budget_name, type, item_name, amount) {
+  const { data, error } = await db
+    .from(table_name)
+    .insert([
+      {
+        User_Name: user_name,
+        Budget_Name: budget_name,
+        Type: type,
+        Item_Name: item_name,
+        Amount: amount,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.error("Insert failed:", error);
+    return null;
+  }
+
+  return data;
+}
+
+// Update Amount in Row by ID
+async function update_row(row_id, updates) {
+  const { data, error } = await db
+    .from(table_name)
+    .update(updates) // { Amount: 250.0 } is solid syntax here for the updates argument
+    .eq("id", row_id)
+    .select();
+
+  if (error) {
+    console.error("Update failed:", error);
+    return null;
+  }
+
+  return data;
+}
+
+// Delete Row by user_name, budget_name, and item_name ###### SHOULD EXPAND THIS TO BE ABLE TO DELETE BUDGET_NAMES IN THEIR ENTIRETY
+async function delete_row(user_name, budget_name, item_name) {
+  const { error } = await db
+    .from(table_name)
+    .delete()
+    .eq("User_Name", user_name)
+    .eq("Budget_Name", budget_name)
+    .eq("Item_Name", item_name);
+
+  if (error) {
+    console.error("Delete failed:", error);
+  }
+}
+
+//get_all();
