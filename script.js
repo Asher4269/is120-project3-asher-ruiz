af52d794cd765a718da7b162208892cb11926739
Asher Ruiz <asherruiz42@gmail.com>
2026-04-09 05:11:12 -0600

    Adjustments and Local Storage Implementation

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ const div_budgets_cat = document.querySelector(".budgets-category");
 const div_savings_cat = document.querySelector(".savings-category");
 const div_income_calc_ = document.querySelector(".income-calculator");
 
+// Input Elements
+const user_name_inp = document.querySelector("#user_name");
+const budget_name_inp = document.querySelector("#budget_name");
+const type_inp = document.querySelector("#type");
+const item_name_inp = document.querySelector("#name");
+const amount_inp = document.querySelector("#amount");
+
 // Crud Buttons
 const button_alter_item = document.querySelector(".alter-item");
 const button_delete_item = document.querySelector(".delete-item");
@@ -89,4 +96,29 @@ async function delete_row(user_name, budget_name, item_name) {
   }
 }
 
+function save_user_info() {
+  localStorage.setItem("user_name", user_name_inp.value);
+  localStorage.setItem("budget_name", budget_name_inp.value);
+
+  get_user_info();
+}
+
+function get_user_info() {
+  let saved_user_name = localStorage.getItem("user_name");
+  let saved_budget_name = localStorage.getItem("budget_name");
+
+  if (saved_user_name === null) {
+    return null;
+  } else if (saved_budget_name === null) {
+    user_name_inp.value = saved_user_name;
+  } else {
+    user_name_inp.value = saved_user_name;
+    budget_name_inp.value = saved_budget_name;
+    budget_name_display.textContent = saved_budget_name;
+  }
+}
+
+get_user_info();
+
+button_add_item.addEventListener("click", save_user_info);
 //get_all();
